611c3608e31b7514f5029e2b51536b290b4c4288
MatsProg <pawelgixer@gmail.com>
2020-04-05 15:50:13 +0200

    Initial commit

---
+++ plik1.py
@@ -0,0 +1,13 @@
+class PC:
+    def main(self):
+        print('Main self started')
+        wartosc = 5
+        wartosc.bit_length()
+        wartosc1 = []
+        wartosc1.append()
+
+klasa1 = PC()
+
+PC.main(klasa1)
+#or
+klasa1.main()
---
+++ plik2.py
@@ -0,0 +1,3 @@
+class plik2
+    def zawartoscpliku2(self):
+        print('plik2 startuje')
