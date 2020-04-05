64d8869284679167582293922346459489a57825
MatsProg <pawelgixer@gmail.com>
2020-04-05 20:55:18 +0200

    update

--- plik1.py
+++ plik1.py
@@ -1,13 +1,65 @@
+from plik2 import * #importuje caly plik
+
+p2k = plik2() #tworze obiekt class pliku2
+p2k.zawartoscpliku2() #wywoluje funkcje/metode z pliku2
+
 class PC:
+    def __init__(self):
+        print('init wykonuje sie bez wywolywania')
+        self.name = 'print name class PC'
+        self.age = 22
     def main(self):
         print('Main self started')
-        wartosc = 5
-        wartosc.bit_length()
-        wartosc1 = []
-        wartosc1.append()
 
+    def update(self):
+        self.age = 40
+
+#najpierw tworzymy obiekt klasa | niegraniczona ilosc obiektow
 klasa1 = PC()
+klasa2 = PC()
+#wywolujemy klase na 2 sposoby
+PC.main(klasa2)
+#   albo
+klasa2.main()
+#dostajemy sie do srodka
+print(klasa2.name)
+#zmiana wartosci np age
+klasa2.age = 14
+print(klasa2.age) #14 a bylo 22
+#def update (albo cokolwiek to tylko nazwa) - po wywolaniu dopiero zmienia dane ktore sa w srodku
 
-PC.main(klasa1)
-#or
-klasa1.main()
+############################################# dziedziczenie- SINGLE LEVEL INHERITANCE
+
+class A: ############################################# nazwa SUPERCLAS
+    def __init__(self):
+        print('superclas init')
+    def AA(self):
+        print('1')
+    def BB(self):
+        print('2')
+class B(A): #################################  odziedzicza funkcje z klasy A - nazwa SUBCLAS
+    def CC(self):
+        print('3')
+    def DD(self):
+        print('4')
+class C(B):     ############################# multi level inheritance
+    def EE(self):
+        print('5')
+class D():
+    def FF(self):
+        print('6')
+class E(A,D): ############################ nazwa MULTIPLE INHERITANCE inheritance z dwoch klas
+    def __init__(self):                 #wywolanie metody E wywoluje tylko E init
+        print('E class init')
+        super().__init__()                #wywoluje init z Superclass
+    def GG(self):
+        print('7')
+
+#a1 = A() #obiekt dla klasy
+#a2 = B() #obiekt dla klasy
+#c3 = C() #obiekt dla klasy
+klasaE = E()
+#a2.AA() #Klasa B - wywołanie metody AA odziedziczonej z klasy A
+#c3.AA() # zawiera wszystkie wczesniejsze metody
+
+
--- plik2.py
+++ plik2.py
@@ -1,3 +1,7 @@
-class plik2
+class plik2:
     def zawartoscpliku2(self):
         print('plik2 startuje')
+
+    def __init__(self):
+        pass #nie moze byc pusta funkcja
+      #  self.name = name
